864da89c64ab1aceab666ace56f27fcc77919415
Lavinia <laviniat1512@gmail.com>
2023-12-04 23:58:39 +0200

    add hamburger navbar for smaller screens

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,30 +1,41 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   template: `
-  <div class="navbar is-dark">
-    <div class="navbar-brand">
-      <a class="navbar-item">
-      <img src="assets/img/cs-logo.png">
-      </a>
-    </div>
+    <div class="navbar is-dark">
+      <div class="navbar-brand">
+        <a class="navbar-item" routerLink="/">
+          <img src="assets/img/cs-logo.png" alt="UBB Computer Science Logo">
+        </a>
+        <!-- Burger icon for mobile -->
+        <a role="button" class="navbar-burger burger" aria-label="menu" (click)="toggleNavbar()">
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
 
-    <!--- menu --->
-    <div class="navbar-menu">
-      <div class="navbar-end">
-        <a class="navbar-item" routerLink="/">Home</a>
-        <a class="navbar-item" routerLink="/courses">Teaching</a>
-        <a class="navbar-item" routerLink="/contact">Contact</a>
+      <!--- menu --->
+      <div class="navbar-menu" [ngClass]="{ 'is-active': isNavbarActive }">
+        <div class="navbar-end">
+          <a class="navbar-item" routerLink="/">Home</a>
+          <a class="navbar-item" routerLink="/courses">Teaching</a>
+          <a class="navbar-item" routerLink="/contact">Contact</a>
+        </div>
       </div>
     </div>
-  </div>
   `,
   styles: ``
 })
 export class HeaderComponent {
+  isNavbarActive = false;
 
+  toggleNavbar() {
+    this.isNavbarActive = !this.isNavbarActive;
+  }
 }
