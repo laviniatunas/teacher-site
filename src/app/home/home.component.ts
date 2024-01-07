d3a06b9d1f36337909620bd81a67d7dac20bdefd
Lavinia <laviniat1512@gmail.com>
2024-01-07 14:07:02 +0200

    adjust home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,16 +7,18 @@ import { Component } from '@angular/core';
   template: `
     <section class="hero is-primary is-bold is-fullheight">
       <div class="hero-body">
-        <div class="container has-text-centered bg-gray p-5"> 
+        <div class="container has-text-centered bg-gray p-5 home-container"> 
           <h1 class="title">
             Welcome on my Page!
           </h1>
           <div class="columns is-mobile is-centered is-vcentered">
-            <div class="column">
+            <div class="column home-image">
               <img src="/assets/img/me.jpg" alt="Description of the image">
             </div>
-            <div class="column">
-              <p class="is-size-7"><strong>Hello there! I'm Ms. Țunaș Lavinia, your assistant teacher at the Faculty of Mathematics and Computer Science, Babeș-Bolyai University in Cluj-Napoca. I will be your guide on this educational journey. Within these virtual walls, we'll explore the exciting world of Computer Science.</strong></p>
+            <div class="column home-text">
+              <p class="is-size-6 has-text-centered">
+                Hello there! I'm Ms. Țunaș Lavinia, your assistant teacher at the Faculty of Mathematics and Computer Science, Babeș-Bolyai University in Cluj-Napoca.
+              </p>
             </div>
           </div>
         </div>
